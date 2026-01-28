f54b84d80897dbcb65ff88cf86bb4351862146da
Luis-cloud356 <luishenriquecravo56@gmail.com>
2026-01-28 16:16:09 -0300

    Novo

---
+++ script.js
@@ -0,0 +1,8 @@
+function obterDados(e){
+     fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    .then(resp => resp.json())
+    .then(resp => {
+        e.innerHTML = resp.USDBRL.bid
+    })
+    
+}
